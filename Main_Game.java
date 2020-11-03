8e4a7b177a68220067736c96a53aa43c494fa690
Daniel <56806846+zhongdaniel@users.noreply.github.com>
2020-11-03 12:25:14 -0500

    Bug Fixes

--- Main_Game.java
+++ Main_Game.java
@@ -4,47 +4,46 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Main_Game extends JFrame{
- //Variables
- static CardLayout cardsL;
- static Container c;
+	//Variables
+	static CardLayout cardsL;
+	static Container c;
 
- Home_Menu homeP;
- Game_Menu gameP;  
- Rules_Menu rulesP;
- Pause_Menu pauseP;
- Result_Menu resultP;
- 
- //Constructor
- public Main_Game(){
-  c = getContentPane();
-  cardsL=new CardLayout();
-  c.setLayout(cardsL);
-     
-  homeP = new Home_Menu();
-  gameP = new Game_Menu();
-  pauseP = new Pause_Menu();
-  rulesP = new Rules_Menu();
-  resultP = new Result_Menu();
-  
-  this.addKeyListener(gameP);
-    
-     c.add("Home", homeP);
-     c.add("Rules", rulesP);
-     c.add("Pause", pauseP);
-     c.add("Results", resultP);
-     c.add("Game", gameP);
-        
- }
- 
- public static void main(String[] args) {
-  Main_Game f = new Main_Game();
-  
-  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  f.setVisible(true);
-  f.setResizable(false);
-  f.setSize(1280,720);
- }
- 
+	Home_Menu homeP;
+	static Game_Menu gameP;  
+	Rules_Menu rulesP;
+	Pause_Menu pauseP;
+	Result_Menu resultP;
+	
+	//Constructor
+	public Main_Game(){
+		c = getContentPane();
+		cardsL=new CardLayout();
+		c.setLayout(cardsL);
+		   
+		homeP = new Home_Menu();
+		gameP = new Game_Menu();
+		pauseP = new Pause_Menu();
+		rulesP = new Rules_Menu();
+		resultP = new Result_Menu();
+		  
+	    c.add("Konosuba: The Ripoff- Home", homeP);
+	    c.add("Konosuba: The Ripoff- Rules", rulesP);
+	    c.add("Konosuba: The Ripoff- Pause", pauseP);
+	    c.add("Konosuba: The Ripoff- Results", resultP);
+	    c.add("Konosuba: The Ripoff", gameP);
+	  	    
+	}
+	
+	public static void main(String[] args) {
+		Main_Game f = new Main_Game();
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.setResizable(false);
+		f.setSize(1280,720);
+	}
+	
+	
 
 }
 
