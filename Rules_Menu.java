d89ce3ada6828e1d5034725446467c0cfe1531b6
Daniel <56806846+zhongdaniel@users.noreply.github.com>
2020-11-03 11:46:41 -0500

    Added Background and Appearance Changes

--- Rules_Menu.java
+++ Rules_Menu.java
@@ -7,19 +7,24 @@ import javax.swing.*;
 public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 	//Variables
 	private JButton goHome;
+	private ImageIcon bg;
 	
 	public Rules_Menu(){  // constructor
 		//Creating the Buttons
-		goHome = new JButton("     Ok     ");
+		goHome = new JButton("Go back");
+		goHome.setFont(new Font("Garamond", Font.BOLD, 16));
+		goHome.setBounds(100, 400, 200, 35);
+		goHome.setBackground(new Color(227, 88, 64));
+		
+		bg = new ImageIcon("Rules_Background.png");
 		
 		//Adding Listeners
 		goHome.addActionListener(this);
 		addKeyListener(this);
 		
 		//Adding Objects
-		this.setLayout(new FlowLayout());
+		this.setLayout(null);
 		this.setBackground(Color.white);
-
 		this.add(goHome);
 		       
 	}	   
@@ -52,7 +57,20 @@ public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
-		g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 18));  
+		
+		//add background
+		g.drawImage(bg.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+		
+		g.fillRect(80, 175, 300, 140);
+
+		g.fillRect(720, 175, 300, 140);
+		g.setColor(java.awt.Color.black);
+		
+				
+		g.setFont(new Font("Garamond", Font.BOLD+Font.ITALIC,  18));
+		g.setColor(java.awt.Color.white);
+		
+		
 		g.drawString("Instructions:",100,200);  
 		g.drawString("Player 1 Controls:",100,225);   
 		g.drawString("Move Character - WASD",100,250);   
@@ -62,6 +80,7 @@ public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 		g.drawString("Move Character - IJKL",740,250);   
 		g.drawString("Attack - U",740,275);   
 		g.drawString("Use Special Ability - O", 740, 300);
+	
 	}
 	
 	
