f8513542219a839d1a96c8ddd68f13670d29ff9e
AntonQi <56732710+AntonQi@users.noreply.github.com>
2020-11-03 09:03:55 -0500

    Update Result_Menu.java

--- Result_Menu.java
+++ Result_Menu.java
@@ -4,28 +4,58 @@ import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class Result_Menu extends JPanel implements ActionListener{
-		private JButton goMain;
-		private JButton goRules;// variables: button
+public class Result_Menu extends JPanel implements ActionListener, KeyListener{
+	private JButton goMain;
+	private JButton goGame;
 		
-		public Result_Menu(){  // constructor
-               goMain=new JButton(" Return to Main Menu ");
-	           goMain.addActionListener(this);
-	           this.setLayout(new FlowLayout());
-	           this.add(goMain);     // add the button
-			   this.setBackground(Color.GREEN);
-		}	   
-	    public void actionPerformed(ActionEvent e) {
-	    	
-	    	if(e.getSource() == goMain) 
-		    	  Main_Game.cardsL.last(Main_Game.c);
-
-
+		
+	public Result_Menu() {
+		//Adding Buttons
+		goMain = new JButton(" Return to Main Menu ");
+	    goGame = new JButton("     Play Again     ");
+	        
+	    
+	    //Adding Listeners
+		goMain.addActionListener(this);
+	    goGame.addActionListener(this);
+	    this.setLayout(new FlowLayout());
+	        
+	    //Adding Objects
+	    this.add(goGame);
+	    this.add(goMain);
+	    this.setBackground(Color.GREEN);
+		
 	}
-	public void paintComponent(Graphics g){
-	   super.paintComponent(g);
-	   g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 18));  // set a new font
-	   g.drawString("Welcome to ~~Summoner's Rift~~ Konosuba: the ripoff",100,200);   // display the string starting at the coordinate (100, 200)
-   }//end of paint   
+	
+	//Key Events
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+					
+	}
 
-}// end of MyMenuPanel class
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+					
+	}
+				
+	public void keyPressed( KeyEvent e ) {
+				
+				
+	}
+	
+	//Action Events
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//Draw Component
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 18));
+		g.drawString("Welcome to ~~Summoner's Rift~~ Konosuba: the ripoff",100,200);
+   }
+
+	
+
+}
