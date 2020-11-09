3918e560e72ccb044f8fb37262aa7ead391b1b61
AntonQi <56732710+AntonQi@users.noreply.github.com>
2020-11-09 09:14:27 -0500

    Update Pause_Menu.java

--- Pause_Menu.java
+++ Pause_Menu.java
@@ -6,71 +6,76 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Pause_Menu extends JPanel implements ActionListener, KeyListener{
- //Variables
- private JButton playButton, goHome;
- private ImageIcon play;
- private Image playImg, sizedImg;
+	//Variables
+	private JButton playButton, restartButton, goHome;
+	private ImageIcon play;
+	private Image playImg, sizedImg;
   
- public Pause_Menu(){  // constructor
-  //Creating the Buttons
-  play = new ImageIcon("Play.png");
-  playImg = play.getImage();
-  sizedImg = playImg.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH); 
-  play = new ImageIcon(sizedImg);
-  
-  playButton = new JButton(play);
-  playButton.setBounds(1200, 10, 50, 50);
-  playButton.setOpaque(false);
-  playButton.setContentAreaFilled(false);
-  playButton.setBorderPainted(false);
-  
-  goHome = new JButton("     Quit     ");
-  goHome.setBounds(100, 350, 200, 35);
-  goHome.setBackground(new Color(227, 88, 64));
-      
-  //Adding Listeners
-  playButton.addActionListener(this);
-  goHome.addActionListener(this);
-  addKeyListener(this);
-  
-  //Adding Objects
-  this.setLayout(null);
-     this.setBackground(Color.GREEN);
-     
-     this.add(playButton);
-  this.add(goHome);
- }
+	public Pause_Menu(){  // constructor
+		//Creating the Buttons
+		play = new ImageIcon("Play.png");
+		playImg = play.getImage();
+		sizedImg = playImg.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH); 
+		play = new ImageIcon(sizedImg);
+		
+		playButton = new JButton(play);
+		playButton.setBounds(1200, 10, 50, 50);
+		playButton.setOpaque(false);
+		playButton.setContentAreaFilled(false);
+		playButton.setBorderPainted(false);
+	
+		goHome = new JButton("     Quit     ");
+		goHome.setBounds(100, 350, 200, 35);
+		goHome.setBackground(new Color(227, 88, 64));
+		
+		//Adding Listeners
+		playButton.addActionListener(this);
+		goHome.addActionListener(this);
+		addKeyListener(this);
+		
+		//Adding Objects
+		this.setLayout(null);
+		this.setBackground(Color.GREEN);
+		
+		this.add(playButton);
+		this.add(goHome);
+	}
  
- //Action Events
- public void actionPerformed(ActionEvent e) {
+	//Action Events
+	public void actionPerformed(ActionEvent e) {
      
-     if(e.getSource() == playButton) 
-      Main_Game.cardsL.show(Main_Game.c, "Game");
+		if (e.getSource() == playButton) 
+			Main_Game.cardsL.show(Main_Game.c, "Game");
      
-     if(e.getSource() == goHome) 
-      Main_Game.cardsL.show(Main_Game.c, "Home");
+		if (e.getSource() == goHome) 
+			Main_Game.cardsL.show(Main_Game.c, "Home");
+     
+		if (e.getSource() == restartButton) {
+			Main_Game.gameP.restartGame();
+			Main_Game.cardsL.show(Main_Game.c, "Game");
+     }
  }
      
- //Key Events
- public void keyTyped(KeyEvent e) {
-  // TODO Auto-generated method stub
+	//Key Events
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
   
- }
+	}
 
- public void keyPressed(KeyEvent e) {
-  // TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
   
- }
+	}
 
- public void keyReleased(KeyEvent e) {
-  // TODO Auto-generated method stub
-  
- }
+	public void keyReleased(KeyEvent e) {	
+		// TODO Auto-generated method stub
+	 
+	}
  
- //Draw Component
- public void paintComponent(Graphics g){
-  super.paintComponent(g);
-  g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 18));
-  g.drawString("Game is paused",200,300);
- }
+	//Draw Component
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 18));
+		g.drawString("Game is paused",200,300);
+	}
 }
