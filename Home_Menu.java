2aa6665dbf5bf6f42dd0289c19167be4cf9dcd38
Daniel <56806846+zhongdaniel@users.noreply.github.com>
2020-11-12 15:53:22 -0500

    Multiple Cosmetic Updates and Fixes

--- Home_Menu.java
+++ Home_Menu.java
@@ -6,51 +6,62 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class Home_Menu extends JPanel implements ActionListener, KeyListener{
 	//Variables
+	private Color buttonColour;
 	private JButton goGame;
 	private JButton goRules;
-	private ImageIcon bg, game, rules;
+	private ImageIcon bg, game, rules, header;
 	private Image gameImg, sizedImg, rulesImg, sizeRules;
 	
 	//Constructor
 	public Home_Menu(){
-		//Creating the Objects
-		game = new ImageIcon("Play_Button.png");
+		
+		//Creating the Play Button
+		game = new ImageIcon("Play_Button.png"); //Importing the Button's Image and Resizing
 		gameImg = game.getImage();
 		sizedImg = gameImg.getScaledInstance(150, 50, java.awt.Image.SCALE_SMOOTH); 
 		game = new ImageIcon(sizedImg);
 		
-		goGame = new JButton(game);
-		goGame.setBounds(150, 380, 150, 50);
+		
+		goGame = new JButton(game); //Turning the image into a button
+		goGame.setBounds(210, 360, 190, 90); //Setting button size and location
 		goGame.setOpaque(false);
 		goGame.setContentAreaFilled(false);
 		goGame.setBorderPainted(false);
 		
-		
-		rules = new ImageIcon("Rules_Button.png");
+		//Creating the Rules Button
+		rules = new ImageIcon("Rules_Button.png"); //Importing the Button's Image and Resizing
 		rulesImg = rules.getImage();
 		sizeRules = rulesImg.getScaledInstance(175, 50, java.awt.Image.SCALE_SMOOTH); 
 		rules = new ImageIcon(sizeRules);
 		
-		goRules = new JButton(rules);
-		goRules.setBounds(145, 450, 175, 50);
+		goRules = new JButton(rules); //Turning the image into a button
+		goRules.setBounds(220, 460, 190, 90); //Setting button size and location
 		goRules.setOpaque(false);
 		goRules.setContentAreaFilled(false);
 		goRules.setBorderPainted(false);
 		
-		bg = new ImageIcon("home_background.png");
+		//Importing the Images for the Title and Background
+		bg = new ImageIcon("home_background.png"); //Background
+		header = new ImageIcon("Header.png"); //TItle
+		
+		//Add colour presets for buttons
+		buttonColour = new Color(255, 255, 255, 100);
 		
 		//Adding Listeners
 		goGame.addActionListener(this);
 		goRules.addActionListener(this);
-		addKeyListener(this);
 		goGame.addKeyListener(this);
 		goRules.addKeyListener(this);
+		
 		
 		//Adding Objects
 	    this.setLayout(null);
 	    this.add(goGame);
 	    this.setBackground(Color.CYAN);
 		this.add(goRules);
+		
+		//Resetting the game
+		Main_Game.restartGameBool = true;
 		
 	}
 	    	
@@ -64,7 +75,7 @@ public class Home_Menu extends JPanel implements ActionListener, KeyListener{
 	}
 				
 	public void keyPressed( KeyEvent e ) {
-		if(e.getKeyCode() == 13) { // 13 is enter
+		if(e.getKeyCode()==13) { // 13 is enter
 			Main_Game.cardsL.show(Main_Game.c, "Game"); 
 	
 		}
@@ -77,19 +88,25 @@ public class Home_Menu extends JPanel implements ActionListener, KeyListener{
 			Main_Game.cardsL.show(Main_Game.c, "Rules"); 
 		    	
 		if(e.getSource() == goGame) {
-			Main_Game.restartGameBool = true;
 			Main_Game.cardsL.show(Main_Game.c, "Game");
 		}
 	}
 	
 	//Draw Component    
 		public void paintComponent(Graphics g) {
+			
 			super.paintComponent(g);
+			
+			//Add the background image
 			g.drawImage(bg.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
 			
+			//Add the title
+			g.drawImage(header.getImage(), 70, 100, 800, 100, null);
 			
-			g.setFont(new Font("Garamond", Font.BOLD, 30));
-			g.drawString("Konosuba: The Game", 100, 300);
+			//Create backgrounds for the buttons
+			g.setColor(buttonColour);
+			g.fillRoundRect(220, 375, 170, 65, 10, 10);
+			g.fillRoundRect(220, 470, 190, 65, 10, 10);
 			
 			
 		}
