bdd2462495f79ca9b28b5a39ee7d033cf86ad0ae
Daniel <56806846+zhongdaniel@users.noreply.github.com>
2020-11-11 23:10:00 -0500

    Update Rules_Menu.java

--- Rules_Menu.java
+++ Rules_Menu.java
@@ -6,32 +6,39 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 	//Variables
-	private JButton goHome;
-	private ImageIcon bg;
+	private JButton backButton;
+	private ImageIcon bg, back;
+	private Image backImg, sizedBack;
 	
 	public Rules_Menu(){  // constructor
 		//Creating the Buttons
-		goHome = new JButton("Go back");
-		goHome.setFont(new Font("Garamond", Font.BOLD, 16));
-		goHome.setBounds(100, 400, 200, 35);
-		goHome.setBackground(new Color(227, 88, 64));
+		back = new ImageIcon("Back_Button.png");
+		backImg = back.getImage();
+		sizedBack = backImg.getScaledInstance(150, 40, java.awt.Image.SCALE_SMOOTH); 
+		back = new ImageIcon(sizedBack);
+		
+		backButton = new JButton(back);
+		backButton.setBounds(100, 350, 150, 40);
+		backButton.setOpaque(false);
+		backButton.setContentAreaFilled(false);
+		backButton.setBorderPainted(false);
   
 		bg = new ImageIcon("Rules_Background.png");
   
 		//Adding Listeners
-		goHome.addActionListener(this);
+		backButton.addActionListener(this);
 		addKeyListener(this);
   
 		//Adding Objects
 		this.setLayout(null);
 		this.setBackground(Color.white);
-		this.add(goHome);
+		this.add(backButton);
          
 	}    
   
 	//Action Events
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == goHome) 
+		if(e.getSource() == backButton) 
 			Main_Game.cardsL.show(Main_Game.c, "Home");
 		
 	}
@@ -67,7 +74,7 @@ public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 		g.setColor(java.awt.Color.black);
   
     
-		g.setFont(new Font("Garamond", Font.BOLD+Font.ITALIC,  18));
+		g.setFont(new Font("Arial", Font.BOLD+Font.ITALIC,  18));
 		g.setColor(java.awt.Color.white);
   
   
@@ -75,11 +82,9 @@ public class Rules_Menu extends JPanel implements ActionListener, KeyListener{
 		g.drawString("Player 1 Controls:",100,225);   
 		g.drawString("Move Character - WASD",100,250);   
 		g.drawString("Attack - Q",100,275);   
-		g.drawString("Use Special Ability - E", 100, 300);
 		g.drawString("Player 2 Controls:",740,225);   
   		g.drawString("Move Character - IJKL",740,250);   
   		g.drawString("Attack - U",740,275);   
-  		g.drawString("Use Special Ability - O", 740, 300);
  
  }
  
