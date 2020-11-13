fba41d2b2e4e08975c2d5b014f77a7696257f881
Daniel <56806846+zhongdaniel@users.noreply.github.com>
2020-11-12 21:41:51 -0500

    Changed Buttons, added comments

--- Pause_Menu.java
+++ Pause_Menu.java
@@ -1,104 +1,100 @@
+/*|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+ * File: Pause_Menu.java
+ * Date Created: Oct 28th
+ * Co-Authors: Anton Qi and Daniel Zhong
+ * Desc: This file's code diplays a screen when the game is paused, contains options to resume game, return
+ * to home page, and to restart game.
+ * |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+ */
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
-
 @SuppressWarnings("serial")
-public class Pause_Menu extends JPanel implements ActionListener, KeyListener{
+public class Pause_Menu extends JPanel implements ActionListener{
 	//Variables
-	private JButton playButton, restartButton, quitButton;
-	private ImageIcon play, quit, restart, bg;
-	private Image playImg, sizedImg, quitImg, sizedQuit, restartImg, sizedRestart;
+	private JButton resumeButton, restartButton, quitButton;
+	private ImageIcon resume, quit, restart, bg;
+	private Image resumeImg, sizedResume, quitImg, sizedQuit, restartImg, sizedRestart;
   
 	public Pause_Menu(){  // constructor
-		//Creating the Buttons
-		play = new ImageIcon("Play.png");
-		playImg = play.getImage();
-		sizedImg = playImg.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH); 
-		play = new ImageIcon(sizedImg);
+		//Creating Buttons
+		//making the resume button
+		resume = new ImageIcon("Resume.png");//importing the image
+		resumeImg = resume.getImage();
+		sizedResume = resumeImg.getScaledInstance(200, 50, java.awt.Image.SCALE_SMOOTH); 
+		resume = new ImageIcon(sizedResume);
 		
-		playButton = new JButton(play);
-		playButton.setBounds(1200, 10, 50, 50);
-		playButton.setOpaque(false);
-		playButton.setContentAreaFilled(false);
-		playButton.setBorderPainted(false);
+		resumeButton = new JButton(resume); //making play symbol button
+		resumeButton.setBounds(105, 250, 200, 50);
+		resumeButton.setOpaque(false);
+		resumeButton.setContentAreaFilled(false);
+		resumeButton.setBorderPainted(false);
 	
-		quit = new ImageIcon("Quit_Button.png");
+		//making the quit button
+		quit = new ImageIcon("Quit_Button.png"); //import quit image
 		quitImg = quit.getImage();
 		sizedQuit = quitImg.getScaledInstance(120, 50, java.awt.Image.SCALE_SMOOTH); 
 		quit = new ImageIcon(sizedQuit);
 		
+		//making the button
 		quitButton = new JButton(quit);
 		quitButton.setBounds(95, 325, 120, 50);
 		quitButton.setOpaque(false);
 		quitButton.setContentAreaFilled(false);
 		quitButton.setBorderPainted(false);
 		
-		restart = new ImageIcon("Restart_Button.png");
+		//making the restart game button
+		restart = new ImageIcon("Restart_Button.png");//import restart image
 		restartImg = restart.getImage();
 		sizedRestart = restartImg.getScaledInstance(350, 50, java.awt.Image.SCALE_SMOOTH); 
 		restart = new ImageIcon(sizedRestart);
 		
+		//making the button
 		restartButton = new JButton(restart);
 		restartButton.setBounds(100, 400, 350, 50);
 		restartButton.setOpaque(false);
 		restartButton.setContentAreaFilled(false);
 		restartButton.setBorderPainted(false);
 		
-		
+		//add background image to a variable
 		bg = new ImageIcon("pauseBg.png");
 		
-		//Adding Listeners
-		playButton.addActionListener(this);
+		//Adding Listeners to buttons
+		resumeButton.addActionListener(this);
 		quitButton.addActionListener(this);
 		restartButton.addActionListener(this);
-		addKeyListener(this);
 		
 		//Adding Objects
 		this.setLayout(null);
-		this.setBackground(Color.GREEN);
 		
-		this.add(playButton);
+		//adding buttons to layout
+		this.add(resumeButton);
 		this.add(quitButton);
 		this.add(restartButton);
 	}
  
 	//Action Events
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) { //define the actions of the buttons
      
-		if (e.getSource() == playButton) 
+		if (e.getSource() == resumeButton) //resumes game
 			Main_Game.cardsL.show(Main_Game.c, "Game");
      
-		if (e.getSource() == quitButton) 
+		if (e.getSource() == quitButton) //return to home page
 			Main_Game.cardsL.show(Main_Game.c, "Home");
      
-		if (e.getSource() == restartButton) {
+		if (e.getSource() == restartButton) { //restart game
 			Main_Game.restartGameBool = true;
 			Main_Game.cardsL.show(Main_Game.c, "Game");
      }
  }
-     
-	//Key Events
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-  
-	}
-
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-  
-	}
-
-	public void keyReleased(KeyEvent e) {	
-		// TODO Auto-generated method stub
-	 
-	}
+    
 	
 	//Draw Component
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		 
+		
+		//add background
 		g.drawImage(bg.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
 	}
 }
